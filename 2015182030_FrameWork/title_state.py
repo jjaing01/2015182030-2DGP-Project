88f64b4441c88b89c5d58c845d47e57434f6d38e
Leedonghee(Notebook) <jjaing01@naver.com>
2019-10-27 02:57:32 +0900

    Tengai 19.10.27_5
    캐릭터 선택창 작업

--- 2015182030_FrameWork/title_state.py
+++ 2015182030_FrameWork/title_state.py
@@ -4,20 +4,30 @@ import main_state
 
 
 name = "TitleState"
-image = None
+image = []
+image_num=None
+iNumber=0
 
 
 def enter():
     global image
-    image = load_image('Tengai/Resource/Menu/Select_Character0.png')
+    global image_num
+    address = 'Tengai/Resource/Menu/Select_Character'
+    extension = '.png'
+    for n in range(0,5):
+        all_address=address+str(n)+extension
+        image_num = load_image(all_address)
+        image.append(image_num)
 
 
 def exit():
     global image
-    del(image)
+    #del(image)
+    image.clear()
 
 
 def handle_events():
+    global iNumber
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -27,12 +37,22 @@ def handle_events():
                 game_framework.quit()
             elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
                 game_framework.change_state(main_state)
+            elif(event.type, event.key) == (SDL_KEYDOWN, SDLK_RIGHT):
+                if iNumber > 3:
+                    iNumber = 0
+                else:
+                    iNumber += 1
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_LEFT):
+                if iNumber < 1:
+                    iNumber = 4
+                else:
+                    iNumber -= 1
 
 def draw():
+    global iNumber
     clear_canvas()
-    image.draw(400, 300)
+    image[iNumber].draw(400, 300)
     update_canvas()
-
 
 
 def update():
